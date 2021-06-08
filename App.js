3b1c80a6cea3d17f41e8f8d7eca2c3c52b94ba54
dextrodeath <ramseyjohnjoseph@gmail.com>
2021-06-08 08:48:35 -0500

    bottom tab nav installed

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BottomTabNav from './src/components/BottomTabNav'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      {/* Selected navTab screen here... */}
+
       <StatusBar style="auto" />
+      <BottomTabNav />
     </View>
   );
 }
@@ -15,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-end'
+    
+  }
 });
